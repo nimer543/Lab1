23e8cb3d4e15593fed0ccbc045f44d5a038625ac
nimer543 <nikita.kliuchnyk@gmail.com/usr/local/bin/python3 /Users/nimer/Documents/programer/to_do_list_main.py/usr/local/bin/python3 /Users/nimer/Documents/programer/to_do_list_main.pygit config --list/usr/local/bin/python3 /Users/nimer/Documents/programer/main.py/usr/local/bin/python3 /Users/nimer/Documents/programer/main.py/usr/local/bin/python3 /Users/nimer/Documents/programer/main.py/usr/local/bin/python3 /Users/nimer/Documents/programer/main.py/usr/local/bin/python3 /Users/nimer/Documents/programer/main.py/usr/local/bin/python3 /Users/nimer/Documents/programer/main.py/usr/local/bin/python3 /Users/nimer/Documents/programer/main.py>
2026-03-20 13:31:38 +0100

    Reorganize Django project structure and update polls app

--- Django/djangotutorial/mysite/settings.py
+++ Django/djangotutorial/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "polls.apps.PollsConfig",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +125,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TIME_ZONE = 'Europe/Warsaw'
+
+# Django Debug Toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- Django/djangotutorial/mysite/urls.py
+++ Django/djangotutorial/mysite/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
+
+# Django Debug Toolbar
+if __import__('django').conf.settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
